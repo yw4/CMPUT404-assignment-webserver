ddf0f6605f7ba4bc9c10da04415ca98b4d7508ce
Yiyang Wang <yiyang3@ualberta.ca>
2020-01-23 22:08:31 -0700

    update content

--- server.py
+++ server.py
@@ -29,50 +29,64 @@ import socketserver
 
 class MyWebServer(socketserver.BaseRequestHandler):
 
-    def send_405(self):
-        self.request.send("HTTP/1.1 405 Method Not Allowed".encode())
+    def send_404(self,path):
+        status = b"HTTP/1.1 404 Not Found\r\n"
+        content = "No webpage was found for the web address: http://127.0.0.1:8080" + path+"\r\n"
+        content_len = ("Content-Length: {}\r\n".format(str(len(content)))).encode()
+        content_type = b"Content-Type: text/html; charset=UTF-8\r\n"
+        body = content.encode()
+        self.request.send(status + content_len+content_type + b'\r\n' + body)
 
-    def handle_file(self, request):
+    def send_405(self,split_data):
+        status =b"HTTP/1.1 405 Method Not Allowed\r\n"
+        content= "Message:The requested resource does not support http method '"+ split_data + "'"
+        content_len = ("Content-Length: {}\r\n".format(str(len(content)))).encode()
+        content_type =b"Content-Type: application/json; charset=utf-8\r\n"
+        body = content.encode()
+        self.request.send(status + content_len + content_type + b'\r\n'+ body)
+
+    def handle_file(self, path):
         try:
           status = b"HTTP/1.1 200 OK\r\n"
-          if request[-4:] == ".css":
-            content = open( "./www"+ request,'r').read()
-            content_type = b"Content-Type: text/css;\r\n\r\n"
+          if path[-4:] == ".css":
+            content = open( "./www"+ path,'r').read()
+            content_type = b"Content-Type: text/css; charset=UTF-8\r\n"
             content_len = ("Content-Length: {}\r\n".format(str(len(content)))).encode()
             body = content.encode()
-            self.request.send(status + content_len+content_type + body)
-          elif request[-5:] == ".html":
-            content = open( "./www"+ request,'r').read()
-            content_type = b"Content-Type: text/html;\r\n\r\n"
+            self.request.send(status + content_len+content_type + b'\r\n' + body)
+          elif path[-5:] == ".html":
+            content = open( "./www"+ path,'r').read()
+            content_type = b"Content-Type: text/html; charset=UTF-8\r\n"
             content_len = ("Content-Length: {}\r\n".format(str(len(content)))).encode()
             body = content.encode()
-            self.request.send(status + content_len+content_type + body)
+            self.request.send(status + content_len+content_type + b'\r\n'+ body)
 
         except:
-          self.request.send("HTTP/1.1 404 Not Found".encode())
+          #self.request.send("HTTP/1.1 404 Not Found".encode())
+          self.send_404(path)
 
     def handle_deep_end(self,path):
         try:
           content = open( "./www"+path+'/index.html','r').read()
           status = b"HTTP/1.1 301 Moved Permanently\r\n"
-          loc = "Location: http://127.0.0.1:8080"+ path+"/"
+          loc = "Location: http://127.0.0.1:8080"+ path+"/\r\n"
           location = loc.encode()
 
           body = content.encode()
-          self.request.send(status + location)
+          self.request.send(status + location + b'\r\n')
 
         except:
-          self.request.send("HTTP/1.1 404 Not Found".encode())
+          #self.request.send("HTTP/1.1 404 Not Found".encode())
+          self.send_404(path)
 
-        pass
     def handle_path(self,path):
         try:
           content = open( "./www"+path+'index.html','r').read()
           status = b"HTTP/1.1 200 OK\r\n"
           content_len = ("Content-Length: {}\r\n".format(str(len(content)))).encode()
-          content_type = b"Content-Type: text/html;\r\n\r\n"
+          content_type = b"Content-Type: text/html; charset=UTF-8\r\n"
           body = content.encode()
-          self.request.send(status + content_len + content_type + body)
+          self.request.send(status + content_len+ content_type + b'\r\n'+ body)
 
         except:
           self.handle_deep_end(path)
@@ -82,17 +96,17 @@ class MyWebServer(socketserver.BaseRequestHandler):
         self.data = self.request.recv(1024).strip()
         #print ("Got a request of: %s\n" % self.data)
         #self.request.sendall(bytearray("OK",'utf-8'))
+        if self.data:
+            split_data = self.data.decode().split()
+            #print(split_data)
 
-        split_data = self.data.decode().split()
-        #print(split_data)
-
-        if split_data[0] == 'GET':
-            if split_data[1][-4:] == '.css' or split_data[1][-5:] == '.html':
-                self.handle_file(split_data[1])
+            if split_data[0] == "GET":
+                if split_data[1][-4:] == '.css' or split_data[1][-5:] == '.html':
+                    self.handle_file(split_data[1])
+                else:
+                    self.handle_path(split_data[1])
             else:
-                self.handle_path(split_data[1])
-        else:
-            self.send_405()
+                self.send_405(split_data[0])
 
 
 if __name__ == "__main__":
